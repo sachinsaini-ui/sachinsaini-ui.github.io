1fa029775f8b1bc65785b3a01f4e6967d04d4efe
Sachin Saini <sachinsaini70405@gmail.com>
2024-07-17 12:20:39 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/json');
 
 // Set the upload directory
-$uploadDir = 'uploads/';
+$uploadDir = 'https://rahultech.me/uploads/';
 if (!is_dir($uploadDir)) {
     mkdir($uploadDir, 0777, true);
 }
@@ -12,7 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['image'])) {
     $uploadFilePath = $uploadDir . basename($file['name']);
 
     if (move_uploaded_file($file['tmp_name'], $uploadFilePath)) {
-        $url = 'https://rahultech.me/' . $uploadFilePath;  // Adjust this URL based on your domain and path
+        $url = 'https://rahultech.me/uploads' . $uploadFilePath;  // Adjust this URL based on your domain and path
         echo json_encode(['success' => true, 'url' => $url]);
     } else {
         echo json_encode(['success' => false, 'message' => 'Failed to move uploaded file.']);
