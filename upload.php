190f317fb8d0e43405470e3f4d87a76026e9f4bc
Sachin Saini <sachinsaini70405@gmail.com>
2024-07-19 19:06:26 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,34 +1,17 @@
 <?php
-header('Content-Type: application/json');
-
 $targetDir = "uploads/";
-if (!is_dir($targetDir)) {
-    mkdir($targetDir, 0777, true);
-}
+$fileName = basename($_FILES["file"]["name"]);
+$targetFilePath = $targetDir . $fileName;
+$response = array();
 
-$targetFile = $targetDir . basename($_FILES["image"]["name"]);
-$imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
-
-// Check if image file is a actual image or fake image
-$check = getimagesize($_FILES["image"]["tmp_name"]);
-if($check !== false) {
-    if (move_uploaded_file($_FILES["image"]["tmp_name"], $targetFile)) {
-        $response = array(
-            "success" => true,
-            "url" => "http://rahultech.me/" . $targetFile
-        );
-    } else {
-        $response = array(
-            "success" => false,
-            "message" => "Sorry, there was an error uploading your file."
-        );
-    }
+// Check if file was uploaded without errors
+if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)) {
+    $response['status'] = 'success';
+    $response['fileName'] = $fileName;
 } else {
-    $response = array(
-        "success" => false,
-        "message" => "File is not an image."
-    );
+    $response['status'] = 'error';
 }
 
+// Return response as JSON
 echo json_encode($response);
 ?>
